6199847cf8a2ea7736aa54aa795fc00bc458e5d2
Valentin Morel <valentin.morel@iliium.com>
2025-01-31 02:24:06 +0100

    Rename confusing deployments.yaml file.

--- scripts/apply-all.sh
+++ scripts/apply-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ../deployments
-kubectl apply -f deployments.yaml
+kubectl apply -f services.yaml
 kubectl apply -f operator.yaml
 kubectl apply -f prometheus.yaml
 kubectl apply -f alertmanager.yaml
